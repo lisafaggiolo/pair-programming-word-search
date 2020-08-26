c35f608979dab19b4e39c109092f1123451570bd
lisafaggiolo <lisa.faggiolo@live.ca>
2020-08-26 22:30:18 +0000

    completed assignment for word search program

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,30 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const transpose = function (matrix) {
+    let transposeArray = [];
+    for (let y = 0; y < matrix.length; y++) {
+      for (let x = 0; x < matrix[y].length; x++) {
+        if (x > transposeArray.length - 1) {
+          transposeArray.push([]);
+        }
+        transposeArray[x].push(matrix[y][x])
+      }
     }
+    return transposeArray;
+  }
+const wordSearch = (letters, word) => {
+    const horizontalJoin = letters.map(ls => ls.join(''))
+    const verticalJoin = transpose(letters).map(ls => ls.join(''))
+    // console.log(verticalJoin);
+    for (let arr of horizontalJoin) {
+        if (arr.includes(word)){
+            return true;
+        }
+    }
+    for (let arrv of verticalJoin) {
+        if (arrv.includes(word)){
+            return true;
+        }
+    }
+    return false
 }
 
 module.exports = wordSearch
